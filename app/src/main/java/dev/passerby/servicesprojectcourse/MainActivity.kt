603df6355237a4ec7d25e973be5f27a3257510f7
passerby29 <grtte@list.ru>
2023-05-09 13:19:28 +0300

    Services start lesson 4

--- app/src/main/java/dev/passerby/servicesprojectcourse/MainActivity.kt
+++ app/src/main/java/dev/passerby/servicesprojectcourse/MainActivity.kt
@@ -1,12 +1,12 @@
 package dev.passerby.servicesprojectcourse
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import dev.passerby.servicesprojectcourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,31 +22,10 @@ class MainActivity : AppCompatActivity() {
             startService(MyService.newIntent(this, 25))
         }
         binding.foregroundService.setOnClickListener {
-            showNotification()
-        }
-    }
-
-    private fun showNotification() {
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_HIGH
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this)
             )
-            notificationManager.createNotificationChannel(notificationChannel)
         }
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Title")
-            .setContentText("Text")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .build()
-        notificationManager.notify(1, notification)
-    }
-
-    companion object{
-
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
     }
 }
---
+++ app/src/main/java/dev/passerby/servicesprojectcourse/MyForegroundService.kt
@@ -0,0 +1,82 @@
+package dev.passerby.servicesprojectcourse
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyForegroundService : Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+        startForeground(1, showNotification())
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        log("onStartCommand")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun log(message: String) {
+        Log.d("ServiceTAG", "MyForegroundService $message")
+    }
+
+    private fun showNotification(): Notification {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+        notificationManager.notify(1, notification)
+        return notification
+    }
+
+    companion object {
+
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MyForegroundService::class.java)
+        }
+    }
+}
