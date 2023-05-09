7cee36277bed0c18262c01729095d5d3cc4a483c
passerby29 <grtte@list.ru>
2023-05-09 13:07:36 +0300

    Services start lesson 3

--- app/src/main/java/dev/passerby/servicesprojectcourse/MainActivity.kt
+++ app/src/main/java/dev/passerby/servicesprojectcourse/MainActivity.kt
@@ -1,7 +1,12 @@
 package dev.passerby.servicesprojectcourse
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import dev.passerby.servicesprojectcourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +21,32 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+        notificationManager.notify(1, notification)
+    }
+
+    companion object{
+
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
